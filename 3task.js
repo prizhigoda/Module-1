acbb933a23f63824a2492961b1e6d46e94bd3e22
Vadim Prizhigoda <prizhigoda.fortech@gmail.com>
2024-05-16 14:48:23 +0300

    rename function, split the line, fix code style

--- 3task.js
+++ 3task.js
@@ -1,7 +1,12 @@
-const input1 = "abctestabctext"
-const input2 = "testabctext"
-const replacingLetters = (string) => {
-    return string.slice(0, 3) === "abc" ? "www" + string.slice(3) : string + "zzz"
-}
-console.log(replacingLetters(input1))
-console.log(replacingLetters(input2))
+const input1 = 'abctestabctext';
+const input2 = 'testabctext';
+
+const replaceLetters = (string) => {
+  const prefix = string.slice(0, 3) === "abc" ? "www" : string;
+  const suffix = string.slice(0, 3) === "abc" ? string.slice(3) : "zzz";
+  
+  return `${prefix}${suffix}`;
+};
+
+console.log(replaceLetters(input1));
+console.log(replaceLetters(input2));
