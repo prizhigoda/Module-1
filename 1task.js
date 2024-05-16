0b95b597053e03ba6aeaad2de1e4c9c33a691993
Vadim Prizhigoda <prizhigoda.fortech@gmail.com>
2024-05-16 11:39:26 +0300

    added a function

--- 1task.js
+++ 1task.js
@@ -1,3 +1,14 @@
-const arrNumber = [6, 43, -6, 3, 0, 5, 2, 7];
-console.log(arrNumber.sort((a,b) => a-b));
-console.log(arrNumber.sort((a,b) => a-b).reverse());
+const elements = [6, 43, -6, 3, 0, 5, 2, 7]
+
+const sortElements = (arr, direction) => {
+  if (direction === 'increasing') {
+    return arr.sort((a, b) => a - b)
+  }
+  else {
+    return arr.sort((a, b) => b - a)
+  }
+  }
+const sortedIncreasing = sortElements(elements, 'increasing')
+console.log(sortedIncreasing)
+const sortedDecreasing = sortElements(elements)
+console.log(sortedDecreasing)
