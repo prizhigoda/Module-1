7b44079ce890becb05b8a5172e8edbd88786b7af
Vadim Prizhigoda <prizhigoda.fortech@gmail.com>
2024-05-16 14:30:54 +0300

    Rename function, fixed code style

--- 2task.js
+++ 2task.js
@@ -1,8 +1,13 @@
-const input1 = 'test education part 2'
-const input2 = 'text'
-const checkArray = (string) => {
-    const result = string.length > 5 ? `${string.slice(0,3)}${string.slice(-3)}` : string[0].repeat(string.length)
-    return result
-} 
-console.log(checkArray(input1))
-console.log(checkArray(input2))
+const input1 = 'test education part 2';
+const input2 = 'text';
+
+const formatResult = (string) => {
+  const result = string.length > 5 
+      ? `${string.slice(0, 3)}${string.slice(-3)}`
+      : string[0].repeat(string.length);
+      
+    return result;
+};
+
+console.log(formatResult(input1));
+console.log(formatResult(input2));
