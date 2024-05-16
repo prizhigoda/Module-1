830a8cdac226d47c5749d61df8fe85e968ff71c8
Vadim Prizhigoda <prizhigoda.fortech@gmail.com>
2024-05-16 11:53:06 +0300

    added function

--- 4task.js
+++ 4task.js
@@ -1,3 +1,6 @@
-const arr = [4, 8, 2, 9, 4, 6, 5, 1, 7, 4]
-let random = Math.floor(Math.random() * arr.length)
-console.log(arr[random])
+const input = [4, 8, 2, 9, 4, 6, 5, 1, 7, 4]
+const getRandomElement = (array) => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+console.log(getRandomElement(input))
