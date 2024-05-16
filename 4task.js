8e5399210484f70e71a1929edc6c10deaabb6c26
Vadim Prizhigoda <prizhigoda.fortech@gmail.com>
2024-05-16 14:50:32 +0300

    fixed code style

--- 4task.js
+++ 4task.js
@@ -1,6 +1,9 @@
-const input = [4, 8, 2, 9, 4, 6, 5, 1, 7, 4]
+const input = [4, 8, 2, 9, 4, 6, 5, 1, 7, 4];
+
 const getRandomElement = (array) => {
-    const randomIndex = Math.floor(Math.random() * array.length);
-    return array[randomIndex];
+  const randomIndex = Math.floor(Math.random() * array.length);
+  
+  return array[randomIndex];
 }
-console.log(getRandomElement(input))
+
+console.log(getRandomElement(input));
