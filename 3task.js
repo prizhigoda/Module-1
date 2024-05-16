378e9ad37d4094d8abdf7eb042a18072c2484ea7
Vadim Prizhigoda <prizhigoda.fortech@gmail.com>
2024-05-16 11:51:38 +0300

    function return, fixed code style

--- 3task.js
+++ 3task.js
@@ -1,8 +1,7 @@
-let str = "abctestabctext"
-let str1 = 'testabctext'
-function check(string){
-    let result = string.slice(0, 3) == "abc" ? "www" + `${string.slice(3)}` : string + "zzz"
-    console.log(result)
+const input1 = "abctestabctext"
+const input2 = "testabctext"
+const replacingLetters = (string) => {
+    return string.slice(0, 3) === "abc" ? "www" + string.slice(3) : string + "zzz"
 }
-check(str)
-check(str1)
+console.log(replacingLetters(input1))
+console.log(replacingLetters(input2))
