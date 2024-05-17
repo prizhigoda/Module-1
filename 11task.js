cd66f7209217f0c1141e3f3241a25a1795fb40f4
Vadim Prizhigoda <prizhigoda.fortech@gmail.com>
2024-05-17 12:45:42 +0300

    added regular expression

--- 11task.js
+++ 11task.js
@@ -1,16 +1,10 @@
 const input1 = 'abcbacabcbcabcbaba';
 const input2 = 'abcbacabcqbcabcbnaba';
 
-const checkSymbol =(str) => {
-  for(const item of str) {
-      if (item !== 'a' && item !=='b'&& item !== 'c'){
-    
-        return false;
-    }
-  }
+const checkSymbol = (str) => {
 
-return true;
-}
+  return /^[abc]+$/.test(str);
+};
 
 console.log(checkSymbol(input1));
 console.log(checkSymbol(input2));
