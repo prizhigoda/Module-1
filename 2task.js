e7e60a292e5971debf256f02d45945ecbd38b531
Vadim Prizhigoda <prizhigoda.fortech@gmail.com>
2024-05-17 11:35:11 +0300

    delete tab

--- 2task.js
+++ 2task.js
@@ -3,8 +3,8 @@ const input2 = 'text';
 
 const formatResult = (string) => {
   const result = string.length > 5 
-      ? `${string.slice(0, 3)}${string.slice(-3)}`
-      : string[0].repeat(string.length);
+    ? `${string.slice(0, 3)}${string.slice(-3)}`
+    : string[0].repeat(string.length);
       
     return result;
 };
