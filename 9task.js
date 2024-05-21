e5c74d7a71c34f89afcd3b86767df9b557e39969
Vadim Prizhigoda <prizhigoda.fortech@gmail.com>
2024-05-21 09:41:08 +0300

    fixed code style

--- 9task.js
+++ 9task.js
@@ -2,10 +2,9 @@ const arr1 = [5, 2, 7, 3, 6, 8, 2, 9, 1];
 const arr2 = [4, 2, 9, 4, 5, 4];
 
 const findSameNumbers = (arr1, arr2) => {
-  let result = [];
+  const result = [];
 
   for (const num of arr1) {
-
     if (arr2.includes(num) && !result.includes(num)) {
       result.push(num);
     }
