b5780386ca3d4bf208909a75a86868a44916d438
Vadim Prizhigoda <prizhigoda.fortech@gmail.com>
2024-05-21 11:18:47 +0300

    rename function

--- 13task.js
+++ 13task.js
@@ -3,12 +3,12 @@ const array = [
   { name: 'height', value: 100 },
 ];
 
-const TransformArrayToObject = (input) => {
-  return (newArray = input.reduce((acc, obj) => {
+const transformArrayToObject = (array) => {
+  return (newArray = array.reduce((acc, obj) => {
     acc[obj.name] = obj.value;
 
     return acc;
   }, {}));
 };
 
-console.log(TransformArrayToObject(array));
+console.log(transformArrayToObject(array));
